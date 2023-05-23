7b6be4264e684e96f8df06fa41c1fe947c52a0c9
HonzaTuron <jan.turon@apify.com>
2023-05-23 17:25:52 +0200

    fix: remove semicolons

--- src/main.py
+++ src/main.py
@@ -14,38 +14,38 @@ async def main():
 
         os.environ['OPENAI_API_KEY'] = actor_input.get('openai_token')
 
-        print("Loading dataset");
+        print("Loading dataset")
         loader = ApifyDatasetLoader(
             dataset_id=actor_input.get('payload')['resource']['defaultDatasetId'],
             dataset_mapping_function=lambda dataset_item: Document(
                 page_content=dataset_item["description"], metadata={"source": dataset_item["url"]}
             ),
         )
-        print("Dataset loaded");
+        print("Dataset loaded")
 
-        print("Loading documents");
+        print("Loading documents")
         documents = loader.load()
-        print("Documents loaded");
+        print("Documents loaded")
 
-        print("Splitting documents");
+        print("Splitting documents")
         text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
         docs = text_splitter.split_documents(documents)
-        print("Documents split");
+        print("Documents split")
 
         PINECONE_API_KEY = actor_input.get('pinecone_token')
         PINECONE_ENV = actor_input.get('pinecone_env')
 
-        print("Initializing pinecone");
+        print("Initializing pinecone")
         pinecone.init(
             api_key=PINECONE_API_KEY,  # find at app.pinecone.io
             environment=PINECONE_ENV  # next to api key in console
         )
-        print("Pinecone initialized");
+        print("Pinecone initialized")
 
         index_name = actor_input.get("index_name")
 
         embeddings = OpenAIEmbeddings()
-        print("Creating index");
+        print("Creating index")
         Pinecone.from_documents(docs, embeddings, index_name=index_name)
-        print("Index created");
+        print("Index created")
 
